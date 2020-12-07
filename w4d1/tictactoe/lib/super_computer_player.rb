1c57ed088e3d7edd544f485c7729b59f479a7b6b
droid4alex <droid4alex@gmail.com>
2020-12-07 13:52:15 -0800

    Finished super_computer_player.rb

--- w4d1/tictactoe/lib/super_computer_player.rb
+++ w4d1/tictactoe/lib/super_computer_player.rb
@@ -2,6 +2,18 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    root_node = TicTacToeNode.new(game.board, mark)
+    children = root_node.children
+    next_child = children[0]
+    children.each do |child_node|
+      if child_node.winning_node?(mark)
+        return child_node.prev_move_pos
+      elsif !child_node.losing_node?(mark)
+        next_child = child_node
+      end
+    end
+    raise 000 if next_child.losing_node?(mark)
+    next_child.prev_move_pos
   end
 end
 
