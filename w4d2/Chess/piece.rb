46eea3d9b6ccf2202e5557f0d9f67a1e282670ae
Reed Haubenstock <rhaubenstock@users.noreply.github.com>
2020-12-08 14:50:13 -0800

    Worked on piece.rb, adds slidable.rb and steppable.rb

--- w4d2/Chess/piece.rb
+++ w4d2/Chess/piece.rb
@@ -1,6 +1,25 @@
 class Piece
-    def initialize
+    # symbol, Board, Array
+    def initialize(color, board, pos)
+        @color = color
+        @board = board
+        @pos = pos
+    end
 
+    def to_s
+        @color.to_s #♙
+    end
+
+    def empty?
+        #should return true if null piece? -> override to true in null piece class
+        false
+    end
+
+    def valid_moves
+        # return array of possible moves
+    end
+
+    def pos=(val)
     end
 
     def inspect
---
+++ w4d2/Chess/slidable.rb
@@ -0,0 +1,21 @@
+module Slidable
+  HORIZONTAL_DIRS = [[0,-1],[0,1]]
+  DIAGONAL_DIRS = [[1,1],[1,-1],[-1,1],[-1,-1]]
+
+  def horizontal_dirs
+    HORIZONTAL_DIRS
+  end
+
+  def diagonal_dirs
+    DIAGONAL_DIRS
+  end
+
+  def moves
+    
+  end
+
+
+
+  
+
+end
