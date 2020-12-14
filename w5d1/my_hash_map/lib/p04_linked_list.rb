96b05376c49fc19c5b5eafa4b2fc13a9a35d2239
Reed Haubenstock <rhaubenstock@users.noreply.github.com>
2020-12-14 15:35:52 -0800

    Finish LinkedList methods in p04_linked_list.rb

--- w5d1/my_hash_map/lib/p04_linked_list.rb
+++ w5d1/my_hash_map/lib/p04_linked_list.rb
@@ -14,14 +14,29 @@ class Node
   end
 
   def remove
-    prev.next = next if prev
-    next.prev = prev if next
+    self.prev.next = self.next 
+    self.next.prev = self.prev 
   end
 end
 
+# ptr -> nil ; add a node =>  ptr -> first_node ; 
+# add another node; ptr -> first_node -> second_node -> ...
+
+# ptr -> sentinal_node (has nil value, nil key, nil prev)
+# add a node:
+# ptr -> sentinal_node -> first_node
+#
+#
+# use sentinal nodes for head and tail -> they never get deleted/removed
+#
+
 class LinkedList
+  include Enumerable
   def initialize
-    
+    @head = Node.new
+    @tail = Node.new
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -30,34 +45,69 @@ class LinkedList
   end
 
   def first
+    @head.next
   end
 
   def last
+    @tail.prev
   end
 
   def empty?
+    first == @tail
   end
 
   def get(key)
+    #have to check each node, 1 by 1
+    # don't need to check head and tail
+    current_node = first
+    while current_node != @tail
+      return current_node.val if current_node.key == key
+      current_node = current_node.next
+    end
+    #implicitly returns nil
   end
 
   def include?(key)
+    current_node = first
+    while current_node != @tail
+      return true if current_node.key == key
+      current_node = current_node.next
+    end
+    false
   end
 
   def append(key, val)
+    # appends to the end
+    # make new node
+    new_node = Node.new(key, val)
+    # set new node next of prev of tail
+    last.next = new_node #last is tail.prev
+    new_node.prev = last
+    # set new node to prev of tail
+    new_node.next = @tail
+    @tail.prev = new_node
   end
 
   def update(key, val)
+    # if node with given key is in linked list 
+    # -> update value to new value
+    each {|node| node.val = val if node.key == key}
   end
 
   def remove(key)
+    each {|node| node.remove if node.key == key}
   end
 
-  def each
+  def each(&prc)
+    current_node = first
+    while current_node != @tail
+      prc.call(current_node)
+      current_node = current_node.next
+    end
   end
 
   # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  end
 end
