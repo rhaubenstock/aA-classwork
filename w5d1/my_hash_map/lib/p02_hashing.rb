c9316ada7d14be413b482e3da504eeeb4932a16d
Reed Haubenstock <rhaubenstock@users.noreply.github.com>
2020-12-14 13:54:22 -0800

    Finish p02_hashing.rb by completing Hash#hash

--- w5d1/my_hash_map/lib/p02_hashing.rb
+++ w5d1/my_hash_map/lib/p02_hashing.rb
@@ -25,6 +25,10 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    # cannot do map
+    # convert hash to array
+    self.to_a
+        .sort #{|pair_1, pair_2| pair_1[0] <=> pair_2[0]}
+        .hash
   end
 end
