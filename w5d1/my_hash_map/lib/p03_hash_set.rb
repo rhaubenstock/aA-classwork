aa2bbb11d20ae041251355630bb2728228feb685
Jadin Press <jadinp@gmail.com>
2020-12-14 14:30:22 -0800

    Add Hash Set class

--- w5d1/my_hash_map/lib/p03_hash_set.rb
+++ w5d1/my_hash_map/lib/p03_hash_set.rb
@@ -7,18 +7,31 @@ class HashSet
   end
 
   def insert(key)
+    bucket = self[key.hash]
+    unless bucket.include?(key)
+      bucket << key
+      @count += 1
+    end
+    resize! if num_buckets == count
   end
 
   def include?(key)
+    bucket = self[key.hash]
+    bucket.include?(key)
   end
 
   def remove(key)
+    bucket = self[key.hash]
+    if bucket.include?(key)
+      bucket.delete(key)
+      @count -= 1
+    end
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -26,5 +39,13 @@ class HashSet
   end
 
   def resize!
+    new_store = Array.new(num_buckets * 2) { Array.new }
+    @store.each do |bucket|
+      bucket.each do |key|
+        new_bucket_idx = key.hash % new_store.length
+        new_store[new_bucket_idx] << key
+      end
+    end
+    @store = new_store
   end
 end
