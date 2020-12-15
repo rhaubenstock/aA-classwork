be4275b6cdc734c18e6ca8a1bef25718d6e3edb2
Reed Haubenstock <rhaubenstock@users.noreply.github.com>
2020-12-14 17:05:04 -0800

    Worked on LRUCache in p06_lru_cache.rb still have issues with ejecting properly

--- w5d1/my_hash_map/lib/p06_lru_cache.rb
+++ w5d1/my_hash_map/lib/p06_lru_cache.rb
@@ -1,6 +1,6 @@
 require_relative 'p05_hash_map'
 require_relative 'p04_linked_list'
-
+require "byebug"
 class LRUCache
   def initialize(max, prc)
     @map = HashMap.new
@@ -14,6 +14,13 @@ class LRUCache
   end
 
   def get(key)
+    if @map.include?(key)
+      node = @map.get(key) 
+      update_node!(node)
+    else
+      calc!(key)
+    end
+    @map.get(key).val
   end
 
   def to_s
@@ -23,13 +30,27 @@ class LRUCache
   private
 
   def calc!(key)
+    # debugger
     # suggested helper method; insert an (un-cached) key
+    @store.append(key, @prc.call(key))
+    # add to hashmap 
+    @map.set(key, @store.last)
+    # we could hit over max size
+    eject! if count >= @max
   end
 
   def update_node!(node)
     # suggested helper method; move a node to the end of the list
+    # first remove node from list
+    # then append node to list
+    @store.remove(node.key)
+    # we still have access to node 
+    @store.append(node.key, node.val)
+    @map.set(node.key, @store.last)
   end
 
   def eject!
+    # removing oldest node
+    @store.remove(@store.first.key)
   end
 end
