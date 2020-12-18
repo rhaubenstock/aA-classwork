c7dc7e31cb59c436636a4288fd2d8c7c4b820d5d
Reed Haubenstock <rhaubenstock@users.noreply.github.com>
2020-12-17 17:01:06 -0800

    Worked on adding validations for ShortenedUrl#user_id and started ShortenedUrl::random_code

--- w5d4/URLShortener/app/models/shortened_url.rb
+++ w5d4/URLShortener/app/models/shortened_url.rb
@@ -1,3 +1,11 @@
 class ShortenedUrl < ApplicationRecord
   validates :short_url, :long_url, presence: true, uniqueness: true
+  validates :user_id, presence: true
+
+  def self.random_code
+    # generating random strings until we find one that is not a short_url in the database
+    # then we return it
+    random_short_url = SecureRandom::urlsafe_base64
+    until !ShortenedUrl.exists?(short_url: random_short_url)
+  end
 end
