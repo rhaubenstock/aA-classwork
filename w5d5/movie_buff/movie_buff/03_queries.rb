a5f027d5cb2829849aaf08d894744ae1ba6f6408
Reed Haubenstock <rhaubenstock@users.noreply.github.com>
2020-12-18 16:14:45 -0800

    Completes 02_queries.rb and starts 03_queries.rb. Also starts polls prj

--- w5d5/movie_buff/movie_buff/03_queries.rb
+++ w5d5/movie_buff/movie_buff/03_queries.rb
@@ -1,24 +1,43 @@
 def what_was_that_one_with(those_actors)
   # Find the movies starring all `those_actors` (an array of actor names).
   # Show each movie's title and id.
-
+  Movie
+    .select(:title, :id)
+    .joins(:actors)
+    .group(:id)
+    .where(actors: {name: those_actors})
+    .having('COUNT(*) = ?', those_actors.length)
 end
 
 def golden_age
   # Find the decade with the highest average movie score.
-
+  Movie
+    .select('CAST(floor(yr/10) AS INT)*10 AS decade')
+    .group('decade')
+    .order('AVG(score) DESC')
+    .limit(1)
+    .first
+    .decade
 end
 
 def costars(name)
   # List the names of the actors that the named actor has ever
   # appeared with.
   # Hint: use a subquery
-
+  Actor
+    .select(:name)
+    .joins('castings AS unnamed_castings ON actors.id = unnamed_castings.id')
+    .joins('castings AS named_castings ON unnamed_castings.movie_id = named_castings.movie_id')
+    .joins('actors AS named_actor ON named_actor.id = named_castings.actor_id')
+    .where('named_actor.name = ?', name)
 end
 
 def actor_out_of_work
   # Find the number of actors in the database who have not appeared in a movie
-
+  Actor
+    .left_outer_joins(:movies)
+    .where(movies: {id: nil})
+    .length
 end
 
 def starring(whazzername)
