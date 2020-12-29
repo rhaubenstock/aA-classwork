b21acce5323fa4cb1734974c2801d6ceb51a18d8
Andrew Curd <andrewc2695@gmail.com>
2020-12-29 15:27:50 -0800

    wrote methods for user controller

--- w6d3/first_routes/app/controllers/users_controller.rb
+++ w6d3/first_routes/app/controllers/users_controller.rb
@@ -5,11 +5,25 @@ class UsersController < ApplicationController
   
     def create
         user = User.new(params.require(:user).permit(:name, :email))
-        user.save!
-        render json: user
+        if user.save
+            render json: user
+        else
+            render json: user.errors.full_messages, status: :unprocessable_entity
+        end
     end
 
     def show
-        render json: params
+        user = User.find_by(id: params[:id])
+        render json: user
+    end
+
+    def update
+        user = User.find(params[:id])
+        user.update!(params.require(:user).permit(:name, :email))
+        render json: user
+    end
+
+    def destroy
+
     end
 end
