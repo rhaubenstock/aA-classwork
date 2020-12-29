3541a6f7859334a4d87a5bd1d54567e48d3964b4
Reed Haubenstock <rhaubenstock@users.noreply.github.com>
2020-12-29 15:07:48 -0800

    Worked on users_controller.rb

--- w6d3/first_routes/app/controllers/users_controller.rb
+++ w6d3/first_routes/app/controllers/users_controller.rb
@@ -1,11 +1,15 @@
 class UsersController < ApplicationController
     def index
-        render plain:'index'
+        render json: User.all
     end
   
     def create
-        render json:params
+        user = User.new(params.require(:user).permit(:name, :email))
+        user.save!
+        render json: user
     end
 
-
+    def show
+        render json: params
+    end
 end
