a7e17309950e105ad60d3997720c644cf52bc330
Andrew Curd <andrewc2695@gmail.com>
2020-12-29 16:50:36 -0800

    wrote assosations and seeds for the database

--- w6d3/art_shares/app/models/artwork.rb
+++ w6d3/art_shares/app/models/artwork.rb
@@ -2,4 +2,17 @@ class Artwork < ApplicationRecord
   validates :title, :image_url, :artist_id, presence: true
   validates :artist_id, uniqueness: {scope: :title}
 
+    belongs_to :artist,
+      primary_key: :id,
+      foreign_key: :artist_id,
+      class_name: :User
+
+    has_many :artwork_shares,
+      primary_key: :id,
+      foreign_key: :artwork_id,
+      class_name: :ArtworkShare
+
+    has_many :shared_viewers,
+      through: :artwork_shares,
+      source: :viewer
 end
--- w6d3/art_shares/db/seeds.rb
+++ w6d3/art_shares/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = User.create([{username: 'joejoejoe'}, {username: 'janejanejane'}, {username: 'somecoolusername'}])
+artworks = Artwork.create([{artist_id: 1,title:'bananas' ,image_url:'bananans.com'},{artist_id: 2,title:'banana-split' ,image_url:'bananansplit.com'}, {artist_id: 3,title:'banana' ,image_url:'banana.com'}])
+artwork_share = ArtworkShare.create([{artwork_id: 1, viewer_id: 2}, {artwork_id: 1, viewer_id: 3}, {artwork_id: 2, viewer_id: 3}, {artwork_id: 3, viewer_id: 1}])
