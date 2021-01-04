c56d3b7e53b1e450709fd482fa14b897ab4ffbd5
Reed Haubenstock <rhaubenstock@users.noreply.github.com>
2021-01-04 14:00:37 -0800

    Adds prj folder and files, worked on user.rb

---
+++ w7d1/ninetynine_cats_2/app/models/user.rb
@@ -0,0 +1,41 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class User < ApplicationRecord
+  validates :username, :session_token, :password_digest, presence: true
+  validates :username, :session_token, uniqueness: true
+  validates :password, length: {minimum: 6}, allow_nil: true
+  
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+    @password = password
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom::urlsafe_base64
+    self.save!
+    self.session_token
+  end
+
+  def is_password?(pw)
+    BCrypt::Password.new(self.password_digest).is_password?(pw)
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+
+    if user && user.password?(pw)
+      user
+    else
+      nil
+    end
+  end
+end
