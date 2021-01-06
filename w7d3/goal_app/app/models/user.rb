7b1d7cbe512400c634d30fd97b4b2acbf6ad6287
Chubbibanana <dongsoo.tchah@gmail.com>
2021-01-06 14:54:31 -0800

    Finished User modelspecs

--- w7d3/goal_app/app/models/user.rb
+++ w7d3/goal_app/app/models/user.rb
@@ -1,2 +1,39 @@
+require 'byebug'
 class User < ApplicationRecord
+    validates :username, :password_digest, presence: true
+    validates :username, uniqueness: true
+    validates :session_token, presence: true, uniqueness: true
+    validates :password, length: { minimum: 6}, allow_nil: true
+
+    has_many :goals,
+        primary_key: :id,
+        foreign_key: :user_id,
+        class_name: :Goal
+
+    attr_reader :password
+
+    after_initialize :ensure_session_token
+
+    def ensure_session_token
+        self.session_token ||= SecureRandom::urlsafe_base64 
+    end
+
+    def password=(pw)
+        self.password_digest = BCrypt::Password.create(pw)
+        @password = pw
+    end
+
+    def is_password?(pw)
+        BCrypt::Password.new(self.password_digest).is_password?(pw)
+    end
+
+    def self.find_by_credentials(username, pw)
+        user = User.find_by(username: username)
+
+        if user && user.is_password?(pw)
+            user
+        else
+            nil
+        end
+    end
 end
--- w7d3/goal_app/spec/models/user_spec.rb
+++ w7d3/goal_app/spec/models/user_spec.rb
@@ -2,11 +2,34 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it {should validate_presence_of(:username)}
-  it {should validate_uniqueness_of(:username)}
   it {should validate_presence_of(:password_digest)}
   it {should validate_length_of(:password).is_at_least(6)}
-
+  it {should validate_presence_of(:session_token)}
+  
   it {should have_many(:goals)}
+
+  subject(:capy) {FactoryBot.create(:user)}
+  it {should validate_uniqueness_of(:session_token)}
+  it {should validate_uniqueness_of(:username)}
+
+  describe "password encryption" do
+    it 'does not save password to database' do
+      User.create!(username: "Brad", password: "BradIsRad")
+      brad = User.find_by(username: "Brad")
+      expect(brad.password).not_to eq("BradIsRad")
+    end
+
+    it 'encrpyts using BCrypt' do
+      expect(BCrypt::Password).to receive(:create)
+      FactoryBot.build(:user)
+    end
+  end
+
+  describe "session token" do
+    it 'should assign a session_token if none is given' do
+      expect(FactoryBot.build(:user).session_token).not_to be_nil
+    end
+  end
 
   describe "#find_by_credentials" do
     context "with valid username and password" do
