d9f48633a2e9857e528b210fd4064cc3b5168919
Reed Haubenstock <rhaubenstock@users.noreply.github.com>
2021-01-11 10:12:24 -0800

    Finishes up user auth

--- w8d1/reddit_clone/app/controllers/sessions_controller.rb
+++ w8d1/reddit_clone/app/controllers/sessions_controller.rb
@@ -24,7 +24,7 @@ class SessionsController < ApplicationController
 
     def destroy
         logout
-
+        redirect_to new_session_url
     end
 
 end
--- w8d1/reddit_clone/app/controllers/users_controller.rb
+++ w8d1/reddit_clone/app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
     end
 
     def create
+        debugger
         @user = User.new(user_params)
         if @user.save
             login!(@user)
