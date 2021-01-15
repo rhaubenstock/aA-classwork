02fe2d69fda678be0cd972d4ce2706792c9f2e15
Qi Zhu <rubybfm1017@gmail.com>
2021-01-15 13:37:08 -0800

    printime function

--- w8d5/intro-to-callback-exercises.js
+++ w8d5/intro-to-callback-exercises.js
@@ -9,18 +9,25 @@ class Clock {
         this.seconds = this.date.getSeconds();
 
         // 3. Call printTime.
-        
+        this.printTime();
+
 
         // 4. Schedule the tick at 1 second intervals.
+        setInterval(this.printTime, 1000);
     }
 
     printTime() {
+
+        let timeStr = [this.hours, this.minutes, this.seconds].join(":");
+
+        console.log(timeStr);
         // Format the time in HH:MM:SS
         // Use console.log to print it.
     }
 
     _tick() {
         // 1. Increment the time by one second.
+        
         // 2. Call printTime.
     }
 }
