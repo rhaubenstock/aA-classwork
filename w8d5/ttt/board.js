c42f17403ee2187523dc0d8c89cc75cf77f5f33a
Reed Haubenstock <rhaubenstock@users.noreply.github.com>
2021-01-15 16:35:01 -0800

    Worked on ttt prj

--- w8d5/ttt/board.js
+++ w8d5/ttt/board.js
@@ -13,11 +13,26 @@ class Board {
     }
 
     won(){
-        const rowWinner = this.grid.some(function(row){
-                    return row[0] !== undefined && row.every(function(ele){
-                        return ele === row[0];
-                    })
-                })
+        const rowWinner = [0,1,2].some(function(row_idx){
+            return this.grid[row_idx][0] !== undefined && [0,1,2].every(function(col_idx){
+                return this.grid[row_idx][0] === this.grid[row_idx][col_idx];
+            });
+        });
         
+
+        const colWinner = [0,1,2].some(function(col_idx){
+            return this.grid[0][col_idx] !== undefined && [0,1,2].every(function(row_idx){
+                return this.grid[0][col_idx] === this.grid[row_idx][col_idx];
+            });
+        });
+
+        const topLeftDiagWinner = this.grid[0][0] != undefined && [0,1,2].every(function(idx){
+            return this.grid[idx][idx] === this.grid[0][0];
+        });
+        //0,2 ; 1,1; 2,0
+        const topRightDiagWinner = this.grid[0][2] != undefined && [0,1,2].every(function(idx){
+            return this.grid[idx][2-idx] === this.grid[0][2];
+        });
+        return rowWinner || colWinner || topLeftDiagWinner || topRightDiagWinner;
     }
 }
