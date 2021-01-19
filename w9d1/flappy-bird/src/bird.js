71014f1279dd65c18aff781d08133909b60563be
AlexeyS <xengawe@gmail.com>
2021-01-19 14:27:22 -0800

    work on Bird class

---
+++ w9d1/flappy-bird/src/bird.js
@@ -0,0 +1,7 @@
+class Bird {
+  constructor(dimensions){
+    this.canvas_height = dimensions.height 
+    this.canvas_width = dimensions.width 
+
+  }
+}
--- w9d1/flappy-bird/src/game.js
+++ w9d1/flappy-bird/src/game.js
@@ -1,6 +1,19 @@
+// const Level = require('./level')
+import Level from './level';
+
+
 export default class FlappyBird {
   constructor(canvas){
     this.ctx = canvas.getContext("2d");
     this.dimensions = { width: canvas.width, height: canvas.height };
+  }
+  
+  animate(){
+    this.level.animate(this.ctx);
+  }
+
+  restart(){
+    this.level = new Level(this.dimensions);
+    this.animate();
   }
 }
