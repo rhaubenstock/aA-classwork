9c25306b1f9a1341126d89004fd36e2b27a6d357
Reed Haubenstock <rhaubenstock@users.noreply.github.com>
2021-01-20 16:11:59 -0800

    Adds asteroids prj

---
+++ w9d2/asteroids/src/moving_object.js
@@ -0,0 +1,8 @@
+function MovingObject(){
+  this.pos = arguments[0].pos;
+  this.vel = arugments[0].vel;
+  this.radius = arugments[0].radius;
+  this.color = arugments[0].color;
+}
+
+module.exports = MovingObject;
