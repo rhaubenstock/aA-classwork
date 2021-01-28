a70b60e3415fc49bd43d4e4e882279cede6eb7d6
Reed Haubenstock <rhaubenstock@users.noreply.github.com>
2021-01-28 14:18:45 -0800

    Adds todoReducer functionality

--- w10d4/todos/frontend/reducers/todos_reducer.js
+++ w10d4/todos/frontend/reducers/todos_reducer.js
@@ -1,7 +1,45 @@
+import { RECEIVE_TODO, RECEIVE_TODOS } from './../actions/todo_actions';
 
 
-const todosReducer = (state = {}, action) => {
+const initialState = {
+  1: {
+    id: 1,
+    title: "wash car",
+    body: "with soap",
+    done: false
+  },
+  2: {
+    id: 2,
+    title: "wash dog",
+    body: "with shampoo",
+    done: true
+  }
+};
+
+
+const todosReducer = (state = initialState, action) => {
+  // debugger
+  state = Object.assign({}, state);
   switch (action.type) {
+    case RECEIVE_TODO:
+      // action = {
+      //   type: "RECEIVE_TODO",
+      //   todo: {id, title, body, done}
+      // }
+      state[action.todo.id] = action.todo;
+      return state;
+    case RECEIVE_TODOS:
+      let newState = {}
+      // action = {
+      //   type: "RECEIVE_TODOS",
+      //   todos: [todo1, todo2, ...]
+      // }
+      action.todos.forEach( todo => {
+        newState[todo.id] = todo;
+      });
+      
+      return newState;
+    
     default:
       return state;
   }
--- w10d4/todos/frontend/todo_redux.jsx
+++ w10d4/todos/frontend/todo_redux.jsx
@@ -2,12 +2,14 @@ import Redux from 'redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store'
-
+import { receiveTodo, receiveTodos } from './actions/todo_actions'
 
 document.addEventListener("DOMContentLoaded", ()=>{
   const root = document.getElementById("content");
   ReactDOM.render(<h1>Todos App</h1>, root);
   window.store = configureStore();
+  window.receiveTodo = receiveTodo;
+  window.receiveTodos = receiveTodos;
 });
 
 
