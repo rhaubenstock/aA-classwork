eb25f19fad73b9bec7ed7256612b4c767c02a047
Reed Haubenstock <rhaubenstock@users.noreply.github.com>
2021-01-29 17:20:35 -0800

    Adds shortcut for passing props in todo_list.jsx

--- w10d5/todos/frontend/components/todos/todo_list.jsx
+++ w10d5/todos/frontend/components/todos/todo_list.jsx
@@ -3,7 +3,10 @@ import TodoListItem from './../todo_list/todo_list_item'
 import TodoListForm from './../todo_list/todo_form'
 
 export default ({todos, dispatchReceiveTodo, dispatchRemoveTodo}) => {
-  const todoList = todos.map( (todo) => (<TodoListItem dispatchRemoveTodo={dispatchRemoveTodo} dispatchReceiveTodo={dispatchReceiveTodo} key={todo.id} todo={ todo } />))
+  //cool trick for passing down props with same key name and variable name
+  // {...{varname}}
+  // from: https://stackoverflow.com/questions/51148064/reacts-props-with-the-same-name-as-their-value
+  const todoList = todos.map( (todo) => (<TodoListItem key={todo.id} {...{todo, dispatchRemoveTodo, dispatchReceiveTodo}} />))
   return (
     <>
       <ul>
