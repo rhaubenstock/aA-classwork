21963c8f920350d768a89e3591c37728b53cab4d
Andrew Curd <andrewcurd@Andrews-MacBook-Pro.local>
2021-02-04 15:17:35 -0800

    wrote user model and create controller

---
+++ w11d4/benchBnB/app/models/user.rb
@@ -0,0 +1,36 @@
+class User < ApplicationRecord
+    validates :email, presence: true, uniqueness: true
+    validates :session_token, presence: true, uniqueness: true
+    validates :password_digest, presence: true
+    validates :password, length: {minimum: 6, allow_nil: true}
+
+    after_intialize: ensure_session_token
+
+    attr_reader :password 
+
+    def self.find_by_credentials(email, pw)
+       user = User.find_by(email: email) 
+       if user && user.is_password?(pw)
+          user
+       end
+    end
+
+    def password=(pw)
+        @password = password
+        user.password_digest = BCrypt::Password.create(pw)
+    end
+
+    def is_password?(pw)
+        BCrypt::Password.new(self.password_digest).is_password?(pw)
+    end
+
+    def reset_session_token
+        self.session_token = SecureRandom::base64
+        self.save!
+        self.session_token
+    end
+
+    def ensure_session_token
+        self.session_token ||= SecureRandom::base64
+    end
+end
